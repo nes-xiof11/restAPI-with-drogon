2c3ac328d90f1079bfcf3bebb7c48d9c23e9e8ab
Eloide Novela <eloide.novela@outlook.com>
2025-03-16 01:34:00 +0200

    fix: fixed issues in user controller

--- src/controllers/api_v1_user.hpp
+++ src/controllers/api_v1_user.hpp
@@ -18,10 +18,10 @@ namespace v1
         public:
             METHOD_LIST_BEGIN
             ADD_METHOD_TO(user::create, "/api/v1/user/", Post, "api::api_token_filter");
-            ADD_METHOD_TO(user::create, "/api/v1/user?user_id={1}", Get, "api::api_token_filter");
-            ADD_METHOD_TO(user::create, "/api/v1/user/list/", Get, "api::api_token_filter");
-            ADD_METHOD_TO(user::create, "/api/v1/user?user_id={1}", Put, "api::api_token_filter");
-            ADD_METHOD_TO(user::create, "/api/v1/user?user_id={1}", Delete, "api::api_token_filter");
+            ADD_METHOD_TO(user::single, "/api/v1/user?user_id={1}", Get, "api::api_token_filter");
+            ADD_METHOD_TO(user::list, "/api/v1/user/list/", Get, "api::api_token_filter");
+            ADD_METHOD_TO(user::update, "/api/v1/user?user_id={1}", Put, "api::api_token_filter");
+            ADD_METHOD_TO(user::remove, "/api/v1/user?user_id={1}", Delete, "api::api_token_filter");
             METHOD_LIST_END
 
             void create(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr&)>&&callback);
