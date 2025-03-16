330b1652d0bbb225c6a45a4417a5a602ba85576d
Eloide Novela <eloide.novela@outlook.com>
2025-03-16 13:42:07 +0200

    stable version

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,9 @@
-#include "models/model.hpp"
-#include "models/user.hpp"
-#include "service/user.hpp"
-
-int main(int argc, char** argv) {
-
-    auto storage = model::get_storage();
-    service::user user_service(storage);
-    model::user created_user =
-    {
-        .name = "Fernando",
-        .nickname = "Azevedo",
-        .email = "fernando.azevedo@gmail.com",
-        .password = "7272gsya",
-    }; 
-
-    user_service.create(created_user);
-    auto users = user_service.get_all();
-    if (not users.has_value())
-        return -1;
-    for (auto &user: users.value())
-    {   
-        std::cout << user.name;
-    }
+#include "configurations/boot.hpp"
+#include <glog/logging.h>
+int main(int argc, char** argv) 
+{
+    google::InitGoogleLogging(argv[0]);
+    google::SetStderrLogging(google::LogSeverity::GLOG_INFO);
+    configuration::boot();
     return 0;
 }
