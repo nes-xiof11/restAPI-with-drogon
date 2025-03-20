9ce7163377b2d1b97a59e474381e11b025d134c1
Eloide Novela <eloide.novela@outlook.com>
2025-03-20 13:01:55 +0200

    feat: added feature in user.create method to encrypt user_password

--- src/service/user.hpp
+++ src/service/user.hpp
@@ -2,6 +2,8 @@
 
 #include "crud.hpp"
 #include "../models/user.hpp"
+#include <exception>
+#include "../util/hash.hpp"
 
 namespace service 
 {   
@@ -11,5 +13,16 @@ namespace service
         public:
             explicit user(Storage &storage): crud<Storage, model::user>(storage)
             { }
+
+            long create(model::user& user) override {
+                try {
+                    user.created_at = this->get_current_datatime();
+                    user.password = util::hash::encode(user.password);
+                    return this->storage.template insert<model::user>(user);
+                } catch (const std::exception& e) {
+                    LOG(ERROR) << e.what();
+                }
+                return (-1);
+            }
     };
 }
